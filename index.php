66212c0e7c4b11b9899738bdbfa5dee96e4fd2d7
Juank de Gorvet <juank@gorvet.com>
2023-12-18 02:37:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,38 +9,66 @@ if (!file_exists('core/Config.php') && file_exists('install.php')){
     exit();
 }
 elseif (!file_exists('core/config.php') && !file_exists('install.php')) {
-   header('Location: 503.php');
-    exit();
+  echo '<html dir="ltr"><head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Error de Configuración</title>
+  <link href="public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="public/css/common.css" rel="stylesheet">
+  <link href="public/css/404/404.css" rel="stylesheet">
+  </head>
+  <body class="h-100" cz-shortcut-listen="true" style="line-height:1.5;">
+  <main class="py-5">
+  <div class="container">
+
+  <div class="row justify-content-center">
+  <div id="p404" class="col-12 col-lg-8 ">
+  <div class="card  ">
+  <span  class="d-block fs-error">404</span>
+  <p class="lead border-bottom">Servicio no disponible</p>
+  <p>Lo sentimos, pero no hemos encontrado el archivo <code>config.php</code>. Se ha intentado iniciar el asistente de instalación sin éxito, probablemente debido a la ausencia del archivo <code>install.php</code>.</p> 
+
+  <p>Por favor, asegúrate de que el archivo <code>install.php</code> exista o intenta crear <code>config.php</code> de forma manual.</p>
+  </div>       
+  </div> 
+  </div>
+  </div>
+  </main>
+
+
+
+  </body></html>';
+  die();
 }
 
 require_once 'core/Config.php';
 require_once 'core/Router.php';
 require_once 'app/controllers/RenderController.php';
- 
- 
+
+
 session_start();
 
 // Generar token CSRF 
 if (!isset($_SESSION['csrf_token']) ) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 } 
- 
+
 $renderController = new RenderController();
 $router = new Router($renderController);
 
 // Lógica para manejar la solicitud según sea AJAX o no
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
     // Si es una solicitud AJAX
-     $params = [
-        'templateName' => 'error',
-        'moduleName' => 'error',
-        'actionName' => $_POST['errorType']
-    ];
-    $router->toRender($params); 
+   $params = [
+    'templateName' => 'error',
+    'moduleName' => 'error',
+    'actionName' => $_POST['errorType']
+];
+$router->toRender($params); 
 } else {
     // Si no es una solicitud AJAX, realizar el enrutamiento normal
     $router->route();
 }
- 
- 
+
+
 
